ededcc2456aae81e3d52ae8b806586f19195b1b9
glopezfrutos <gus.lopezfrutos@gmail.com>
2022-04-29 00:36:49 -0300

    ADD --- Part 5. Ball in motion

--- script.js
+++ script.js
@@ -6,11 +6,12 @@
         this.game_over = false;
         this.bars = [];
         this.ball = null;
+        this.playing = false;
     }
 
     self.Board.prototype = {
         get elements() {
-            var elements = this.bars;
+            var elements = this.bars.map(function (bar) { return bar; });
             elements.push(this.ball);
             return elements;
         }
@@ -26,9 +27,16 @@
         this.speed_y = 0;
         this.speed_x = 3;
         this.board = board;
+        this.direction = 1;
 
         board.ball = this;
         this.kind = "circle";
+    }
+    self.Ball.prototype = {
+        move: function () {
+            this.x += (this.speed_x * this.direction);
+            this.y += (this.speed_y);
+        }
     }
 })();
 
@@ -77,8 +85,11 @@
             }
         },
         play: function () {
-            this.clean();
-            this.draw();
+            if (this.board.playing) {
+                this.clean();
+                this.draw();
+                this.board.ball.move();
+            }
         }
     }
 
@@ -89,7 +100,7 @@
                 break;
             case "circle":
                 ctx.beginPath();
-                ctx.arc(element.x, element.y, element.radius, 0,7);
+                ctx.arc(element.x, element.y, element.radius, 0, 7);
                 ctx.fill();
                 ctx.closePath();
                 break;
@@ -106,19 +117,26 @@ var ball = new Ball(350, 100, 10, board);
 
 
 document.addEventListener("keydown", function (ev) {
-    ev.preventDefault();
     if (ev.keyCode == 38) {
+        ev.preventDefault();
         barLeft.up();
     } else if (ev.keyCode == 40) {
+        ev.preventDefault();
         barLeft.down();
     } else if (ev.keyCode == 87) {
+        ev.preventDefault();
         barRight.up();
     } else if (ev.keyCode == 83) {
+        ev.preventDefault();
         barRight.down();
+    } else if (ev.keyCode == 32) {
+        ev.preventDefault();
+        board.playing = !board.playing;
     }
-    console.log("barL: " + barLeft + "\nbarR: " + barRight);
+    // console.log("barL: " + barLeft + "\nbarR: " + barRight);
 });
 
+board_view.draw();
 window.requestAnimationFrame(controller);
 
 function controller() {
