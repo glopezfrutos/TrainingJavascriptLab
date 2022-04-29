f5ccf023138f3cae20b1bb5931b3c6681d14bfd5
glopezfrutos <gus.lopezfrutos@gmail.com>
2022-04-29 01:27:14 -0300

    ADD --- Part 6. Collision

--- script.js
+++ script.js
@@ -28,6 +28,9 @@
         this.speed_x = 3;
         this.board = board;
         this.direction = 1;
+        this.bounce_angle = 0;
+        this.max_bounce_angle = Math.PI / 12;
+        this.speed = 3;
 
         board.ball = this;
         this.kind = "circle";
@@ -36,6 +39,26 @@
         move: function () {
             this.x += (this.speed_x * this.direction);
             this.y += (this.speed_y);
+        },
+        get width() {
+            return this.radius * 2;
+        },
+        get height() {
+            return this.radius * 2;
+        },
+        collision: function (bar) {
+            // Reacciona a la colisión
+            var relative_intersect_y = ( bar.y + (bar.height / 2) ) - this.y;
+
+			var normalized_intersect_y = relative_intersect_y / (bar.height / 2);
+
+			this.bounce_angle = normalized_intersect_y * this.max_bounce_angle;
+			console.log(this.bounce_angle);
+			this.speed_y = this.speed * -Math.sin(this.bounce_angle);
+			this.speed_x = this.speed * Math.cos(this.bounce_angle);
+
+			if(this.x > (this.board.width / 2)) this.direction = -1;
+			else this.direction = 1;
         }
     }
 })();
@@ -84,14 +107,47 @@
                 draw(this.ctx, el)
             }
         },
+        check_collisions: function () {
+            for (var i = this.board.bars.length - 1; i >= 0; i--) {
+                var bar = this.board.bars[i];
+                if (hit(bar, this.board.ball)) {
+                    this.board.ball.collision(bar);
+                }
+            }
+
+        },
         play: function () {
             if (this.board.playing) {
                 this.clean();
                 this.draw();
+                this.check_collisions();
                 this.board.ball.move();
             }
         }
     }
+
+    function hit(a, b) {
+        // a colisiona con b?
+        var hit = false;
+        // colisión horizontal
+        if (b.x + b.width >= a.x && b.x < a.x + a.width) {
+            // colisión vertical
+            if (b.y + b.height >= a.y && b.y < a.y + a.height)
+                hit = true;
+        }
+        // colisión de a con b
+        if (b.x <= a.x && b.x + b.width >= a.x + a.width) {
+            if (b.y <= a.y + b.height >= a.y + a.height)
+                hit = true;
+        }
+        // colisión de b con a
+        if (a.x <= b.x && a.x + a.width >= b.x + b.width) {
+            if(a.y <= b.y && a.y + a.height >= b.y + b.height)
+            hit = true;
+        }
+        return hit;
+    }
+
 
     function draw(ctx, element) {
         switch (element.kind) {
